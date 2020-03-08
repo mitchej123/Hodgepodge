5ce1b36f345c8c364930261ebecfd6034fcafcdd
Jason Mitchell <mitchej@gmail.com>
2020-03-08 11:08:06 -0700

    Nether fences only connect to nether fences

--- src/main/java/com/mitchej123/hodgepodge/fixfenceconnections/mixins/block/MixinBlockFence.java
+++ src/main/java/com/mitchej123/hodgepodge/fixfenceconnections/mixins/block/MixinBlockFence.java
@@ -3,7 +3,6 @@ package com.mitchej123.hodgepodge.fixfenceconnections.mixins.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFence;
 import net.minecraft.block.material.Material;
-import net.minecraft.init.Blocks;
 import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -18,8 +17,8 @@ public class MixinBlockFence {
     public boolean canConnectFenceTo(IBlockAccess world, int x, int y, int z) {
         Block block = world.getBlock(x, y, z);
         return block == (BlockFence)(Object) this
-            || block instanceof net.minecraft.block.BlockFence && block != Blocks.nether_brick_fence
+            || block instanceof net.minecraft.block.BlockFence && block.blockMaterial == ((BlockFence)(Object)this).blockMaterial
             || block instanceof net.minecraft.block.BlockFenceGate
-            || ((block.blockMaterial.isOpaque() && block.renderAsNormalBlock()) && block.blockMaterial != Material.gourd);
+            || block.blockMaterial.isOpaque() && block.renderAsNormalBlock() && block.blockMaterial != Material.gourd;
     }
 }
